a8ffe63baf912cb0e35fe5953b974f47c1fa9575
why <Haoyang.Wu@marsladder.com.au>
2025-04-29 17:46:02 +0800

    feat: Add Jest configuration and setup files for testing

---
+++ jest.setup.ts
@@ -0,0 +1,4 @@
+import "@testing-library/jest-native/extend-expect";
+
+// Global mocks can be added here if needed later
+// e.g., jest.mock("react-native-mmkv", () => ({ /* mock implementation */ }));
